4ef8cf00543b70e45ebf45f36681effe69028839
Mukhammaddiyor Jaloldinov <diyor.jaloldinov@gmail.com>
2025-10-18 21:44:10 +0200

    added groq api key, added my first ai project

---
+++ project1/main.py
@@ -0,0 +1,38 @@
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq  # Groq requires a free API key from groq.com
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def main():
+    model = ChatGroq(model="llama-3.1-8b-instant", temperature=0)
+
+    tools = []
+  
+    agent_executor = create_react_agent(model,tools)
+    print("\nWelcome! i am your Ai assistant. type 'quit' to exit.")
+    print("\nyou can chat with me about anything you like or ask me to perform calculations for you.")
+
+    while True:
+        user_input = input("\nYou: ").strip()
+        if user_input.lower() == 'quit':
+            print("\nGoodbye!")
+            break
+        
+        print("\nAsistant: ", end="")
+        for chunk in agent_executor.stream(
+           {"messages": [HumanMessage(content=user_input)]}
+        ):
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+
+        print()
+
+
+if __name__ == "__main__":
+    main()                    
+           
